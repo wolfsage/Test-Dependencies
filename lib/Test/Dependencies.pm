84160390828156707ac00aa47891cc0163c6f311
Matthew Horsfall <WolfSage@gmail.com>
2013-11-09 08:51:49 -0500

    Refactor a bit, document, add some tests, clean up bad file

--- lib/Test/Dependencies.pm
+++ lib/Test/Dependencies.pm
@@ -1,21 +1,15 @@
 package Test::Dependencies;
-# ABSTRACT: Check that dependencies are properly listed
+# ABSTRACT: Test that dependencies are properly listed for your dist
 
 use strict;
 use warnings;
 
 use Test::More;
-use Test::Pod;
 
-use FindBin qw($Bin);
+use Test::Pod; # all_pod_files
 
 use CPAN::Meta;
 use Module::CoreList;
-
-use File::Spec;
-use File::Find;
-
-use Data::Dumper;
 
 use PPI;
 use Perl::PrereqScanner;
@@ -23,39 +17,58 @@ use Perl::PrereqScanner;
 use Carp qw(croak);
 use Cwd;
 
-my $corelist;
+use version 0.77;
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(all_dependencies_ok);
 
 sub all_dependencies_ok {
-	my ($opt) = @_;
-
-	if ($opt) {
-		if ($opt eq 'allow_corelist') {
-			$corelist = 1;
-		} else {
-			croak("Unknown option $opt in import");
-		}
+	if (@_ % 2) {
+		croak("Bad options, HASH expected");
 	}
 
-	my $rootdir = getcwd;
+	my (%opt) = @_;
 
-	my $meta = get_meta($rootdir) or die "Failed to find our meta modules\n";
+	my $corelist;
+	my %ignore;
 
-	my $pre = $meta->effective_prereqs;
-	my $req = $pre->merged_requirements();
-	my %listed = map { $_ => 1 } $req->required_modules;
+	if ($corelist = delete $opt{corelist}) {
+		if (!version::is_lax($corelist)) {
+			croak("Bad version passed to 'corelist'");
+		}
+
+		$corelist = version->parse($corelist);
+	}
+
+	if ($opt{ignore}) {
+		if (ref $opt{ignore} ne 'ARRAY') {
+			croak("Option 'ignore' must be an ARRAYREF");
+		}
+
+		%ignore = map { $_ => 1 } @{ delete $opt{ignore} };	
+	}
+
+	# Run as an author test by default unless someone explicitly
+	# asks otherwise
+	if (! delete $opt{force}) {
+		require Test::DescribeMe;
+		Test::DescribeMe->import('author');
+	}	
+
+	if (%opt) {
+		croak("Unknown options: " . Dumper(\%opt));
+	}
+
+	my $meta = get_meta(getcwd) or die "Failed to find our meta modules\n";
+
+	my %listed = map { $_ => 1 }
+		$meta->effective_prereqs->merged_requirements->required_modules;
 
 	# Find all of our files
 	my @files = all_pod_files('.');
 
-	my $scanner = Perl::PrereqScanner->new();
-
-	my %ignore;
-
-	# First, assemble list of our packages
+	# First, assemble list of our packages to ignore them
 	for my $f (@files) {
 		next if $f =~ /\.pod$/; # Skip pod
 
@@ -68,6 +81,9 @@ sub all_dependencies_ok {
 			$ignore{$pkg} = 1;
 		}
 	}
+
+	# Scan them!
+	my $scanner = Perl::PrereqScanner->new();
 
 	my %done;
 
@@ -87,61 +103,140 @@ sub all_dependencies_ok {
 
 			# Hmm not found, should we ignore core modules?
 			if (!$ok && $corelist) {
-				$ok = Module::CoreList->first_release($req);
-				$cl = " [corelist]";
+				my $fr = Module::CoreList->first_release($req);
+
+				if ($fr && version->parse($fr) <= $corelist) {
+					$ok = 1;
+					$cl = " [corelist]";
+				}
 			}
 
-			ok($ok, "[$f] Module $req is listed in prereqs$cl");
+			ok($ok, "[$f] Module '$req' is listed in prereqs$cl");
 		}
 	}		
 
 	ok(1, "Finished");
-}
 
-sub file_from_module {
-	my ($mod) = @_;
-	$mod =~ s#::|'#/#g;
-	$mod .= ".pm";
-	return $mod;
-}
-
-sub module_from_file {
-	my ($file) = @_;
-	$file =~ s#/#::#g;
-	$file =~ s/\.pm$//;
-	return $file;
+	done_testing;
 }
 
 sub get_meta {
 	my ($root) = @_;
 
-	if (my @meta = find_mymeta_file($root)) {
-		my $meta;
+	# Look for MYMETA.* and then see if we can parse at least one of
+	# them. These should have been freshly built during the build
+	# phase and contain our requirements.
+	opendir(my $dhand, $root) or die "Failed to open $root: $!\n";
 
-		my %errors;
-
-		for my $m (@meta) {
-			eval { $meta = CPAN::Meta->load_file($m) };
-			if (!$@) {
-				return $meta;
-			}
-			$errors{$m} = $@;
-		}
-
-		die "Failed to parse MYMETA files: " . Dumper(\%errors);
-	}
-}
-
-sub find_mymeta_file {
-	my ($dir) = @_;
-
-	opendir(my $dhand, $dir) or die "Failed to open $dir: $!\n";
-
-	my @metas = grep { /^MYMETA\./ } readdir($dhand);
+	my @meta = grep { /^MYMETA\./ } readdir($dhand);
 
 	closedir($dhand);
 
-	return @metas;
+	my $meta;
+
+	my %errors;
+
+	for my $m (@meta) {
+		eval { $meta = CPAN::Meta->load_file($m) };
+		if (!$@) {
+			return $meta;
+		}
+		$errors{$m} = $@;
+	}
+
+	die "Failed to parse MYMETA files: " . Dumper(\%errors);
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Test::Dependencies - Test that dependencies are properly listed for your dist
+
+=head1 SYNOPSIS
+
+Check that your module lists all external depdencies, including those that are 
+part of the Perl core:
+
+  use Test::Dependencies;
+  all_dependencies_ok();
+
+Ignore dependencies that are listed as core for version 5.8.8 and above:
+
+  all_dependencies_ok(corelist => '5.8.8');
+
+Ignore specific dependencies:
+
+  all_dependencies_ok(ignore => [qw(Foo Bar::Baz)]);
+
+Force tests to be run (since it only expects to be run as an author test by 
+default):
+
+  all_dependencies_ok(force => 1);
+
+=head1 DESCRIPTION
+
+This module provides an author test (by default it will only run if author 
+testing was explicitly requested) to CPAN distributions that will check that 
+all modules used (detected by L<Perl::PrereqScanner>) in tests, utilities, 
+and packages are listed in the distribution's dependencies.
+
+It does this by scanning the distribution directory tree and comparing the 
+requested modules to the generated C<MYMETA.*> that were created at build time 
+for the distribution being tested.
+
+Note that this does NOT detect minimum version requirements.
+
+=head2 Methods
+
+=head3 all_dependencies_ok
+
+  all_dependencies_ok(%options);
+
+Basic usage is (in some .t on its own):
+
+  use Test::Dependencies;
+  all_dependencies_ok();
+
+This will check that B<any> module being loaded B<only by files in the 
+distribution> are listed in C<MYMETA.*>, including those that are part of the 
+Perl core. (It's good form to list core modules as dependencies as its possible 
+they may be evicted in the future.)
+
+The following options may be used to control the behavior:
+
+=over 4
+
+=item B<* force>
+
+  all_dependencies_ok(force => 1);
+
+Set this to true to force the tests to run, even if author testing isn't 
+requested. This is almost certainly a bad idea since it's up to the author to 
+ensure dependencies are listed properly before putting a dist on CPAN.
+
+=item B<* ignore>
+
+  all_dependencies_ok(ignore => [qw(Some::Module Some::Other::Module)]);
+
+Explicilty ignore failures of certain modules. This is also almost certainly a 
+bad idea, but who am I to judge?
+
+=item B<* corelist>
+
+  all_dependencies_ok(corelist => '5.8.8');
+
+This will cause all modules that are listed as being a part of the Perl core 
+from 5.8.8 and up. Note that this doesn't presently check whether or not its 
+SINCE been deprecated, which is almost certainly a bug. TODO: Fix bug.
+
+The versions can be anything that L<version/"is_lax"> accepts.
+
+=back
+
+=head1 AUTHOR
+
+Matthew Horsfall (alh) - <WolfSage@gmail.com>
+
+=cut
